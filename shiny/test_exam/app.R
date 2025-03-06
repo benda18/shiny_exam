415a7f58850ae49d4ea8508891ef843481763fc8
Tim Bender <bendertj@gmail.com>
2025-03-05 23:20:03 -0500

    shiny app

--- shiny/test_exam/app.R
+++ shiny/test_exam/app.R
@@ -1,42 +1,75 @@
+renv::use(
+  openssl     = "openssl@2.3.2",
+  renv        = "renv@1.1.1",
+  shiny       = "shiny@1.10.0"
+)
+
 
 library(shiny)
+library(openssl)
+library(renv)
+
+#renv::embed()
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Old Faithful Geyser Data"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot")
-        )
+  
+  # Application title
+  titlePanel("Preposterous Examination"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(width = 6, # default = 4
+                 fluidRow(
+                   radioButtons(inputId = "Q1", 
+                                label   = "1) What is the airspeed velocity of an unladen swallow??", 
+                                choices = list("roughly 24 miles per hour"         = "A",
+                                               "an African or a European swallow?" = "B")) 
+                 ),
+                 fluidRow(
+                   radioButtons(inputId = "Q2",
+                                label   = "2) How much sawdust can you put into a Rice Krispie Treat before people start to notice?",
+                                choices = list("None"     = "A",
+                                               "Not much" = "B",
+                                               "So Much"  = "C",
+                                               "LOL"      = "D"))
+                 )
+                 
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      #plotOutput("distPlot")
+      "Hash Output:",
+      textOutput("hashTxt"),
+      
+      
     )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  
+  # output$distPlot <- renderPlot({
+  #     # generate bins based on input$bins from ui.R
+  #     # x    <- faithful[, 2]
+  #     # bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  # 
+  #     # # draw the histogram with the specified number of bins
+  #     # hist(x, breaks = bins, col = 'darkgray', border = 'white',
+  #     #      xlab = 'Waiting time to next eruption (in mins)',
+  #     #      main = 'Histogram of waiting times')
+  # })
+  
+  output$hashTxt <- renderText({
+    hashOfResponses <- openssl::sha256(paste(input$Q1, input$Q2, sep = "", collapse = ""))
+    
+  })
+  
+  
+  
+  
 }
 
 # Run the application 
