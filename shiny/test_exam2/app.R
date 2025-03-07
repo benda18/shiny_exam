35b9a3087040ab8f46fbfe64b4c018fbd9e9e068
Tim Bender <bendertj@gmail.com>
2025-03-07 13:04:26 -0500

    questions updated and layout

--- shiny/test_exam2/app.R
+++ shiny/test_exam2/app.R
@@ -45,91 +45,85 @@ library(openssl)
 ui <- fluidPage(
   
   # PANEL: title----
-  titlePanel("Student Exam - Hashed Responses", 
+  titlePanel("Student Exam - Hashed Responses",
              windowTitle = "Hashing Exams in Shiny"),
   
-  
   sidebarLayout(
+    
     # PANEL: Sidebar----
     sidebarPanel(width = 4,
-                 wellPanel(
-                   fluidRow(strong("Display hashing function")),
-                 ),
-                 wellPanel(
-                   fluidRow(strong("Display hash output")),
-                 )
-    ),
+                 wellPanel(fluidRow(
+                   strong("Display hashing function")
+                 ),),
+                 wellPanel(fluidRow(
+                   strong("Display hash output")
+                 ),)),
+    
     # PANEL: Main----
     mainPanel(width = 8,
               wellPanel(
-                tabsetPanel(id = NULL, selected = NULL,
-                            type = "pills", #c("tabs", "pills", "hidden"), 
-                            header = NULL, footer = NULL,
-                            tabPanel(title = "Exam Questions", 
-                                     
-                            ),
-                            tabPanel(title = "Exam Evaluation", 
-                                     
-                            ),
-                            tabPanel(title = "<hold>", 
-                                     
-                            ),
+                tabsetPanel(
+                  id = NULL, selected = NULL, type = "pills", header = NULL,footer = NULL,
+                  
+                  # PANEL: Tab Questions----
+                  tabPanel(
+                    title = "Exam Questions",
+                    fluidRow(h3("Questions:")),
+                    
+                    # question 1----
+                    fluidRow(
+                      radioButtons(
+                        inputId = "Q1",
+                        label   = "1) What is the airspeed velocity of an unladen swallow??",
+                        choices = list("roughly 24 miles per hour"         = "A",
+                                       "an African or a European swallow?" = "B"),
+                        selected = character("0")
+                      )
+                    ),
+                    
+                    # question 2----
+                    fluidRow(
+                      radioButtons(
+                        inputId = "Q2",
+                        label   = "2) How much sawdust can you put into a Rice Krispie Treat before people start to notice?",
+                        choices = list("None"     = "A",
+                                       "Not much" = "B",
+                                       "So Much"  = "C",
+                                       "LOL"      = "D"),
+                        selected = character("0")
+                      )
+                    ),
+                    
+                    # question 3----
+                    fluidRow(
+                      radioButtons(
+                        inputId = "Q3",
+                        label   = "3) Would you rather have bionic arms or bionic legs?",
+                        choices = list("Arms"  = "A",
+                                       "Legs"  = "B",
+                                       "What?" = "C"),
+                        selected = character("0")
+                      )
+                    ),
+                    
+                    # question 4----
+                    fluidRow(
+                      radioButtons(
+                        inputId = "Q4",
+                        label   = "4) Is a hotdog in a bun a sandwich?",
+                        choices = list("No"  = "A",
+                                       "Yes" = "B"),
+                        selected = character("0")
+                      )
+                    ),
+                  ),
+                  
+                  # PANEL: Tab Evaluation----
+                  tabPanel(title = "Exam Evaluation",),
+                  
+                  # PANEL: Tab hold for future use----
+                  tabPanel(title = "<hold>",),
                 ),
-                fluidRow(h3("Questions:")),
-                # question 1----
-                fluidRow(
-                  radioButtons(inputId = "Q1", 
-                               label   = "1) What is the airspeed velocity of an unladen swallow??", 
-                               choices = list("roughly 24 miles per hour"         = "A",
-                                              "an African or a European swallow?" = "B"), 
-                               selected = character("0")) 
-                ),
-                # question 2----
-                fluidRow(
-                  radioButtons(inputId = "Q2",
-                               label   = "2) How much sawdust can you put into a Rice Krispie Treat before people start to notice?",
-                               choices = list("None"     = "A",
-                                              "Not much" = "B",
-                                              "So Much"  = "C",
-                                              "LOL"      = "D"), 
-                               selected = character("0"))
-                ), 
-                # question 3----
-                fluidRow(
-                  radioButtons(inputId = "Q3",
-                               label   = "3) Would you rather have bionic arms or bionic legs?",
-                               choices = list("Arms"  = "A",
-                                              "Legs"  = "B",
-                                              "What?" = "C"), 
-                               selected = character("0"))
-                ),
-                # question 4----
-                fluidRow(
-                  radioButtons(inputId = "Q4",
-                               label   = "4) Is a hotdog in a bun a sandwich?",
-                               choices = list("No"  = "A",
-                                              "Yes" = "B"), 
-                               selected = character("0"))
-                ), 
-                # fluidRow(
-                #   checkboxGroupInput(inputId = "Q5", 
-                #                      label   = "5) Choose all numbers less than 10", 
-                #                      choices = list("13" = "A", 
-                #                                     "9"  = "B", 
-                #                                     "pi" = "C", 
-                #                                     "42" = "D"), 
-                #                      selected = c("B", "C"), 
-                #                      inline   = FALSE)
-                # ),
-                # fluidRow(
-                #   dateInput(inputId   = "Q6", 
-                #             label     = "6) On what date was the declaration of independence signed?", 
-                #             value     = "1776-07-04", 
-                #             min       = "1700-01-01", 
-                #             max       = Sys.Date(), 
-                #             startview = "decade", 
-                #             format    = "MM d, yyyy")
-                # )
               ),
     )
   )
@@ -138,8 +132,7 @@ ui <- fluidPage(
 # Server
 server <- function(input, output) {
   
-  
 }
 
-# Run the application 
+# Run the application
 shinyApp(ui = ui, server = server)
